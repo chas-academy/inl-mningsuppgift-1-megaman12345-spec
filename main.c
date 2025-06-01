3ce6abafdc22fb05dc5c7b5078da024e62100262
eddiiie_91@hotmail.com <eddiiie_91@hotmail.com>
2025-06-01 03:10:59 +0200

    First try

--- main.c
+++ main.c
@@ -3,11 +3,26 @@
 
 int main()
 {
-    //  The Line below must *NOT* be changed or test will fail!
+    // The Line below must *NOT* be changed or test will fail!
     srand(1);
 
-   
-    // Your code goes here
 
+    int kast[100];
+    int antal[6] = {0};  // antal för varje tärningssida
+    int total = 0;
+
+    
+    for (int i = 0; i < 100; i++) {
+        int slag = rand() % 6 + 1;
+        kast[i] = slag;
+        antal[slag - 1]++;
+        total += slag;
+    }
+    for (int i = 0; i < 6; i++) {
+        printf("%d\n", antal[i]); // ndast siffror, ingen text
+    }
+
+    printf("%d\n", total); // Summan
+    printf("%.1f\n", (float)total / 100); // Medelvärdet, 1 Decimal
     return 0;
 }
